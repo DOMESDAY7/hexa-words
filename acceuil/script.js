658ddef35b7e1e8f9bfe23d3416a6582937a93c9
DOMESDAY7 <79838340+DOMESDAY7@users.noreply.github.com>
2021-10-20 20:22:38 +0200

    changement du soir

---
+++ acceuil/script.js
@@ -0,0 +1,17 @@
+"use strict"
+let idiot = document.querySelector(".titreIdiot");
+// taille du navigateur
+let width = window.innerWidth;
+//si il s'agit d'un téléphone ce code ne sera pas pris en compte
+let idiotON=false;
+if (width > 800) {
+    
+   idiot.onmouseover=function(){
+    idiotON=true;
+    idiot.innerHTML="#1D1075";
+   }
+}
+document.querySelector(".button").addEventListener("click",setTimeout(function(){
+    document.querySelector('.content').style.marginTop='-100vh';
+}),2000)
+//tant que le curseur est sur l'élément ayant la class titreIdiot faire idiot.innerHTML
