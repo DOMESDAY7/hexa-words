013be750b4589d538737c3d0b6939959ad4bff0c
DOMESDAY7 <79838340+DOMESDAY7@users.noreply.github.com>
2021-10-29 15:48:49 +0200

    changement

--- acceuil/script.js
+++ acceuil/script.js
@@ -11,7 +11,7 @@ if (width > 800) {
     idiot.innerHTML="#1D1075";
    }
 }
-document.querySelector(".button").addEventListener("click",setTimeout(function(){
-    document.querySelector('.content').style.marginTop='-100vh';
-},2000))
+// document.querySelector(".button").addEventListener("click",setTimeout(function(){
+//     document.querySelector('.content').style.marginTop='-100vh';
+// },2000))
 //tant que le curseur est sur l'élément ayant la class titreIdiot faire idiot.innerHTML
