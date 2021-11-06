17345792f0b0fb13af47383b029bfae1092666e1
DOMESDAY <m.andriamiraho@gmail.com>
2021-11-06 20:50:34 +0100

    changement du soir

--- choice/index.php
+++ choice/index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -9,18 +10,23 @@
     <link rel="stylesheet" href="style.css">
     <link href="https://fonts.googleapis.com/css2?family=Questrial&display=swap" rel="stylesheet">
 </head>
+
 <body>
+    
+    <span class="content_rand">
+    <button class="rand_button">randomize</button>
+    </span>
+    <form method="GET" action="../write/index.php">
+        <input type="text" name="id_perso" class="hide">
+        <span class="content_choose">
+        <button type="submit" class="choose_button">Chooses <br><img src="arrow.svg" alt=""></button>
+        </span>
+    </form>
+    <section class="content_person"><img src="./personnage/person1.png" alt="personnage" class="person"></section>
     <img src="blob.svg" alt="" class="blob1">
     <img src="blob.svg" alt="" class="blob2">
-    <div class="content">
-    <button class="rand_button">randomize</button>
-    <form method="GET" action="../write/index.php">
-    <input type="text" name="id_perso" class="hide">
-    <button type="submit" class="choose_button">Chooses <br><img src="arrow.svg" alt=""></button>
-    </form>
-    <img src="./personnage/person1.png" alt="personnage" class="person">
-    </div>
     <script src="script.js "></script>
-   
+
 </body>
+
 </html>
