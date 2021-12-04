2248e7a7ad0b2f036e5ba7bfe7e874da6b878b5b
DOMESDAY <m.andriamiraho@gmail.com>
2021-12-04 10:16:34 +0100

    rajout de la page about

---
+++ view/aboutUs.php
@@ -0,0 +1,21 @@
+<body>
+    <h1>hexa#word project</h1>
+    <h5 class="student">student : <a href="#portfolio"><span>Mathieu Andriamiraho</span></a></h5>
+    <main>
+        <p class="context">
+            The hexa#words project was created in the framework of the DUT MMI by the teacher Gaëlle Charpentier. The result of this project is to create a website combining a word and a color. The word in question is the hexadecimal counterpart for the color.
+        </p>
+        
+        <article>
+        <h5>The concept</h5>
+        <p>
+            The word chosen for the hexa#word project is the word "idiots". Declined under the following color:
+        </p>
+            <div class="carre"></div>
+            <code>#1D1075</code>
+        </article>
+        <p class="concept">
+            This word and this color will be put in the form of a site where it will be possible to choose a character in a random way and to give him a name as well as an anecdote. Indeed, the Internet users who want to, will tell in the manner of the site <a href="https://www.viedemerde.fr/">www.viedemerde.fr</a> a moment when they felt stupid. To implement this, the user would navigate between several pages.
+        </p>
+    </main>
+</body>
--- view/acceuil.php
+++ view/acceuil.php
@@ -1,12 +1,15 @@
 <body>
-    <div class="content">
+    <main class="content">
         <h1>I am an<br></h1>
         <h1 class="titreIdiot"> Idiot</h1>
         <section class="content_button">
            <a href="?page=choice" class="button link1">Tell my idiocy</a><br>
             <a href="?page=see" class="button link2">See others the idiots</a>
         </section>
-    </div>
+    </main>
+    <footer>
+    <a href="?page=aboutUs" class="about">About us</a>
+    </footer>
 </body>
 
 </html>
