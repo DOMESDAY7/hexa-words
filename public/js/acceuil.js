a04193963b6f512a617a49c8990028b420fc8028
DOMESDAY7 <79838340+DOMESDAY7@users.noreply.github.com>
2021-12-11 00:20:51 +0100

    animation idiot fonctionnelle

--- public/js/acceuil.js
+++ public/js/acceuil.js
@@ -1,53 +1,59 @@
 let idiot = document.querySelector(".titreIdiot");
 let hexa = document.querySelector(".hexa");
-let str = document.querySelectorAll(".titreIdiot>span");
+let spans = document.querySelectorAll(".titreIdiot>span");
 let cpt = 0;
-let content_titre= document.querySelector(".content_titre")
+let letterContent = document.querySelector(".titreIdiot")
 let textIdiot = {
-    hexa: ["1", "D", "1", "0", "7", "5"],
+    hexa: ["#1", "D", "1", "0", "7", "5"],
     word: ["I", "d", "i", "o", "t", "s"]
 }
-let interval = setInterval(() => {
-    goDownChangeLetter(str[cpt]);
-    
-    cpt++;
-    if (cpt == str.length) {
-        clearInterval(interval)
-    }
-
-}, 200);
-
-function goDownChangeLetter(el) {
-    // el.style.backgroundColor ="red"
-    console.log(el)
-    el.animate([{
-                top: '0'
-            },
-            {
-                top: //taille du conteneur titre
-            }
-
-        ], {
-            duration: 500
+let check=true;
+setInterval(() => {
+    let interval = setInterval(() => {
+        goDown(spans[cpt]);
+       setTimeout(changeLetter, 1300,spans[cpt]);
+        cpt++;
+        if (cpt == spans.length) {
+            clearInterval(interval)
+            cpt=0;
         }
+    
+    }, 100);
+    if(check){
+        check=false;
+    }else{
+        check=true;
+    }
+}, 2000);
 
-    )
+function changeLetter(el){
+    // console.log(el)
+    if (check){
+        let arr = Array.prototype.slice.call(spans);
+        let place = arr.indexOf(el)
+        el.textContent = textIdiot.word[place];
+        
+
+       
+    }else{
+        let arr = Array.prototype.slice.call(spans);
+        let place = arr.indexOf(el)
+        el.textContent = textIdiot.hexa[place]
+       
+    }
    
-    setTimeout(() => {
-        // el.textContent = textIdiot.hexa[cpt]
-        el.style.top="80px"
-    }, 499);
-    // el.animate([{
-    //             top: '80px'
-    //         },
-    //         {
-    //             top: '0'
-    //         }
-
-    //     ], {
-    //         duration: 1000
-    //     }
-
-    // )
-
+}
+function goDown(el) {
+    el.animate([{
+        top: '0'
+    }, {
+        top: `${letterContent.offsetHeight}px`
+    },{
+        top: `${letterContent.offsetHeight*2}px`
+    },{
+        top: '0'
+    }
+], {
+        duration: 500,delay:1000
+    })
 }
